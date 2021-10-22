8069468a3dea0037fb82e42c27a6cb64edce9ff7
Matt <matthv@gmail.com>
2021-10-22 11:18:44 +0200

    chore: add new config option 'route_prefix'

--- config/forest.php
+++ config/forest.php
@@ -2,5 +2,6 @@
 
 return [
     'models_directory' => env('MODEL_DIRECTORY', 'app/Models/'),
-    'json_file_path'   => env('JSON_FILE_PATH', '.forestadmin-schema.json')
+    'json_file_path'   => env('JSON_FILE_PATH', '.forestadmin-schema.json'),
+    'route_prefix'     => env('FOREST_ROUTE_PREFIX', 'forest'),
 ];
