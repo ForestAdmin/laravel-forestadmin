f730d32a0cf72dda871dedc8c951a566b729b794
Matt <matthv@gmail.com>
2021-10-27 11:06:29 +0200

    chore: new exception class

---
+++ src/Exceptions/ForestApiException.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace ForestAdmin\LaravelForestAdmin\Exceptions;
+
+/**
+ * Class ForestApiException
+ *
+ * @package Laravel-forestadmin
+ * @license GNU https://www.gnu.org/licenses/licenses.html
+ * @link    https://github.com/ForestAdmin/laravel-forestadmin
+ */
+class ForestApiException extends \RuntimeException
+{
+}
