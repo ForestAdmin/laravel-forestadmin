9ad5b40b81be8c877d5a93c87a98928da2ac7c65
Matt <matthv@gmail.com>
2021-11-02 09:18:08 +0100

    chore: set new env config 'auth-secret'

--- config/forest.php
+++ config/forest.php
@@ -3,8 +3,9 @@
 return [
     'models_directory' => env('MODEL_DIRECTORY', 'app/Models/'),
     'json_file_path'   => env('JSON_FILE_PATH', '.forestadmin-schema.json'),
-    'api'   => [
-        'url'              => env('FOREST_URL', 'https://api.forestadmin.com'),
-        'secret'           => env('FOREST_ENV_SECRET'),
+    'api'              => [
+        'url'         => env('FOREST_URL', 'https://api.forestadmin.com'),
+        'secret'      => env('FOREST_ENV_SECRET'),
+        'auth-secret' => env('FOREST_AUTH_SECRET'),
     ]
 ];
