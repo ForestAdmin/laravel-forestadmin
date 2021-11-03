28f013966964e57f8f7d415f15f7db40065d54e3
Matt <matthv@gmail.com>
2021-11-03 09:46:56 +0100

    feat(login): add test on ForestResourceOwner

---
+++ tests/Unit/ForestResourceOwnerTest.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace ForestAdmin\LaravelForestAdmin\Tests\Unit;
+
+use Firebase\JWT\JWT;
+use ForestAdmin\LaravelForestAdmin\Auth\OAuth2\ForestResourceOwner;
+use ForestAdmin\LaravelForestAdmin\Tests\TestCase;
+
+/**
+ * Class ForestProviderTest
+ *
+ * @package Laravel-forestadmin
+ * @license GNU https://www.gnu.org/licenses/licenses.html
+ * @link    https://github.com/ForestAdmin/laravel-forestadmin
+ */
+class ForestResourceOwnerTest extends TestCase
+{
+    /**
+     * @var ForestResourceOwner
+     */
+    private ForestResourceOwner $forestResourceOwner;
+
+    /**
+     * @return void
+     */
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->forestResourceOwner = new ForestResourceOwner(
+            [
+                'type'                              => 'users',
+                'id'                                => '1',
+                'first_name'                        => 'John',
+                'last_name'                         => 'Doe',
+                'email'                             => 'jdoe@forestadmin.com',
+                'teams'                             => [
+                    0 => 'Operations'
+                ],
+                'tags'                              => [],
+                'two_factor_authentication_enabled' => false,
+                'two_factor_authentication_active'  => false,
+            ],
+            1234
+        );
+    }
+
+    /**
+     * @return void
+     */
+    public function testGetId(): void
+    {
+        $this->assertEquals(1, $this->forestResourceOwner->getId());
+    }
+
+    /**
+     * @return void
+     */
+    public function testToArray(): void
+    {
+        $this->assertIsArray($this->forestResourceOwner->toArray());
+        $this->assertArrayHasKey('id', $this->forestResourceOwner->toArray());
+        $this->assertArrayHasKey('email', $this->forestResourceOwner->toArray());
+        $this->assertArrayHasKey('first_name', $this->forestResourceOwner->toArray());
+        $this->assertArrayHasKey('last_name', $this->forestResourceOwner->toArray());
+        $this->assertArrayHasKey('teams', $this->forestResourceOwner->toArray());
+        $this->assertArrayHasKey('tags', $this->forestResourceOwner->toArray());
+        $this->assertArrayHasKey('two_factor_authentication_enabled', $this->forestResourceOwner->toArray());
+        $this->assertArrayHasKey('two_factor_authentication_active', $this->forestResourceOwner->toArray());
+    }
+
+    /**
+     * @return void
+     */
+    public function testExpirationInSeconds(): void
+    {
+        $this->assertIsInt($this->forestResourceOwner->expirationInSeconds());
+    }
+
+    /**
+     * @return void
+     */
+    public function testMakeJwt(): void
+    {
+        $result = JWT::decode($this->forestResourceOwner->makeJwt(), config('forest.api.auth-secret'), array('HS256'));
+        $this->assertEquals(1, $result->id);
+    }
+}
