dff3e1a9060afcefefd2a14df670505e4dbbb55f
Matt <matthv@gmail.com>
2021-11-08 16:41:12 +0100

    chore: add test on AuthController & AuthManager

---
+++ tests/Feature/AuthControllerTest.php
@@ -0,0 +1,157 @@
+<?php
+
+namespace ForestAdmin\LaravelForestAdmin\Tests\Unit;
+
+use Firebase\JWT\JWT;
+use ForestAdmin\LaravelForestAdmin\Auth\AuthManager;
+use ForestAdmin\LaravelForestAdmin\Exceptions\ForestApiException;
+use ForestAdmin\LaravelForestAdmin\Http\Controllers\AuthController;
+use ForestAdmin\LaravelForestAdmin\Tests\TestCase;
+use ForestAdmin\LaravelForestAdmin\Utils\ErrorMessages;
+use GuzzleHttp\Exception\GuzzleException;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Routing\Route;
+use League\OAuth2\Client\Provider\Exception\IdentityProviderException;
+use Prophecy\Argument;
+use Prophecy\PhpUnit\ProphecyTrait;
+use Mockery as m;
+
+/**
+ * Class AuthControllerTest
+ *
+ * @package Laravel-forestadmin
+ * @license GNU https://www.gnu.org/licenses/licenses.html
+ * @link    https://github.com/ForestAdmin/laravel-forestadmin
+ */
+class AuthControllerTest extends TestCase
+{
+    use ProphecyTrait;
+
+    /**
+     * @var AuthController
+     */
+    private AuthController $authController;
+
+    /**
+     * @return void
+     */
+    public function setUp(): void
+    {
+        parent::setUp();
+    }
+
+    /**
+     * @throws GuzzleException
+     * @throws \JsonException
+     * @return void
+     */
+    public function testLogin(): void
+    {
+        $params = ['renderingId' => 1];
+        $request = Request::create('/forest/authentication', 'POST', $params);
+        app()->instance('request', $request);
+
+        $auth = $this->prophesize(AuthManager::class);
+        $auth
+            ->start(route('forest.auth.callback'), 1)
+            ->shouldBeCalled()
+            ->willReturn(
+                ['authorizationUrl' => 'http://localhost/oidc/auth?state=%7B%22renderingId%22%3A28%7D&scope=openid%20profile%20email&response_type=code&approval_prompt=auto&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fforest%2Fauthentication%2Fcallback&client_id=TEST']
+            );
+
+        $this->authController = new AuthController($auth->reveal());
+        $this->authController->login();
+    }
+
+    /**
+     * @throws GuzzleException
+     * @throws \JsonException
+     * @throws IdentityProviderException
+     * @return void
+     */
+    public function testCallback(): void
+    {
+        $user = [
+            'id'           => 1,
+            'email'        => 'john.doe@example.com',
+            'first_name'   => 'John',
+            'last_name'    => 'Doe',
+            'team'         => 'Operations',
+            'tags'         => [
+                0 => [
+                    'key'   => 'demo',
+                    'value' => '1234',
+                ],
+            ],
+            'rendering_id' => 1,
+            'exp'          => (new \DateTime())->modify('+ 1 hour')->format('U'),
+        ];
+        $jwt = JWT::encode($user, config('forest.api.auth-secret'));
+
+        $auth = $this->prophesize(AuthManager::class);
+        $auth
+            ->verifyCodeAndGenerateToken(route('forest.auth.callback'), Argument::any())
+            ->shouldBeCalled()
+            ->willReturn($jwt);
+
+        $this->authController = new AuthController($auth->reveal());
+        $callback = $this->authController->callback();
+
+        $this->assertInstanceOf(JsonResponse::class, $callback);
+        $content = json_decode($callback->getContent(), true);
+
+        $this->assertArrayHasKey('token', $content);
+        $this->assertArrayHasKey('tokenData', $content);
+    }
+
+    /**
+     * @throws \ReflectionException
+     * @return void
+     */
+    public function testGetAndCheckRenderingId(): void
+    {
+        $params = ['renderingId' => 1];
+        $request = Request::create('/forest/authentication', 'POST', $params);
+        app()->instance('request', $request);
+
+        $this->authController = app(AuthController::class);
+        $this->assertIsInt($this->invokeMethod($this->authController, 'getAndCheckRenderingId'));
+    }
+
+    /**
+     * @throws \ReflectionException
+     * @return void
+     */
+    public function testGetAndCheckExceptionMissingRenderingId(): void
+    {
+        $params = [];
+        $request = Request::create('/forest/authentication', 'POST', $params);
+        app()->instance('request', $request);
+
+        $this->expectException(ForestApiException::class);
+        $this->expectExceptionMessage(ErrorMessages::MISSING_RENDERING_ID);
+
+        $this->authController = app(AuthController::class);
+        $this->assertIsInt($this->invokeMethod($this->authController, 'getAndCheckRenderingId'));
+    }
+
+
+    /**
+     * @throws \ReflectionException
+     * @return void
+     */
+    public function testGetAndCheckExceptionInvalidRenderingId(): void
+    {
+        $params = ['renderingId' => 10.1];
+        $request = Request::create('/forest/authentication', 'POST', $params);
+        app()->instance('request', $request);
+
+        $this->expectException(ForestApiException::class);
+        $this->expectExceptionMessage(ErrorMessages::INVALID_RENDERING_ID);
+
+        $this->authController = app(AuthController::class);
+        $this->assertIsInt($this->invokeMethod($this->authController, 'getAndCheckRenderingId'));
+    }
+}
---
+++ tests/Unit/AuthManagerTest.php
@@ -0,0 +1,198 @@
+<?php
+
+namespace ForestAdmin\LaravelForestAdmin\Tests\Unit;
+
+use ForestAdmin\LaravelForestAdmin\Auth\AuthManager;
+use ForestAdmin\LaravelForestAdmin\Auth\OAuth2\ForestProvider;
+use ForestAdmin\LaravelForestAdmin\Auth\OAuth2\ForestResourceOwner;
+use ForestAdmin\LaravelForestAdmin\Auth\OidcClientManager;
+use ForestAdmin\LaravelForestAdmin\Exceptions\ForestApiException;
+use ForestAdmin\LaravelForestAdmin\Tests\TestCase;
+use ForestAdmin\LaravelForestAdmin\Utils\ErrorMessages;
+use Illuminate\Contracts\Container\BindingResolutionException;
+use League\OAuth2\Client\Token\AccessToken;
+use Prophecy\Argument;
+use Prophecy\PhpUnit\ProphecyTrait;
+use Prophecy\Prophecy\ObjectProphecy;
+
+/**
+ * Class AuthManagerTest
+ *
+ * @package Laravel-forestadmin
+ * @license GNU https://www.gnu.org/licenses/licenses.html
+ * @link    https://github.com/ForestAdmin/laravel-forestadmin
+ */
+class AuthManagerTest extends TestCase
+{
+    use ProphecyTrait;
+
+    /**
+     * @return void
+     */
+    public function setUp(): void
+    {
+        parent::setUp();
+    }
+
+    /**
+     * @throws BindingResolutionException
+     * @return void
+     */
+    public function testStart(): void
+    {
+        app()['config']->set('app.debug', false);
+        $return = '123ABC';
+        $oidc = $this->makeOidc($return, true);
+
+        /** @var AuthManager auth */
+        $this->auth = app()->make(AuthManager::class);
+        $this->auth->oidc = $oidc->reveal();
+
+        $start = $this->auth->start('localhost/foo', 1);
+        parse_str(parse_url($start, PHP_URL_QUERY), $output);
+
+        $this->assertSame($output['state'], '{"renderingId":1}');
+    }
+
+    /**
+     * @throws BindingResolutionException
+     * @return void
+     */
+    public function testVerifyCodeAndGenerateToken(): void
+    {
+        app()['config']->set('app.debug', false);
+        $return = '123ABC';
+        $oidc = $this->makeOidc($return);
+
+        /** @var AuthManager auth */
+        $this->auth = app()->make(AuthManager::class);
+        $this->auth->oidc = $oidc->reveal();
+
+        $data = ['code' => 'test', 'state' => '{"renderingId":1}'];
+        $token = $this->auth->verifyCodeAndGenerateToken('mock_host/foo', $data);
+
+        $this->assertIsString($token);
+        $this->assertSame($return, $token);
+    }
+
+    /**
+     * @throws BindingResolutionException
+     * @throws \ReflectionException
+     * @return void
+     */
+    public function testGetRenderingIdFromState(): void
+    {
+        /** @var AuthManager auth */
+        $this->auth = app()->make(AuthManager::class);
+        $data = ['state' => '{"renderingId":1}'];
+        $renderingId = $this->invokeMethod($this->auth, 'getRenderingIdFromState', $data);
+
+        $this->assertIsInt($renderingId);
+        $this->assertSame($renderingId, 1);
+    }
+
+    /**
+     * @throws BindingResolutionException
+     * @throws \ReflectionException
+     * @return void
+     */
+    public function testGetRenderingIdFromStateException(): void
+    {
+        /** @var AuthManager auth */
+        $this->auth = app()->make(AuthManager::class);
+        $data = ['state' => '{"renderingId":10.1}'];
+        $this->expectException(ForestApiException::class);
+        $this->expectExceptionMessage(ErrorMessages::INVALID_STATE_FORMAT);
+        $this->invokeMethod($this->auth, 'getRenderingIdFromState', $data);
+    }
+
+    /**
+     * @throws BindingResolutionException
+     * @throws \ReflectionException
+     * @return void
+     */
+    public function testStateIsValid(): void
+    {
+        /** @var AuthManager auth */
+        $this->auth = app()->make(AuthManager::class);
+        $data = ['code' => 'test', 'state' => '{"renderingId":1}'];
+        $state = $this->invokeMethod($this->auth, 'stateIsValid', array(&$data));
+        $this->assertTrue($state);
+    }
+
+    /**
+     * @throws BindingResolutionException
+     * @throws \ReflectionException
+     * @return void
+     */
+    public function testStateException(): void
+    {
+        /** @var AuthManager auth */
+        $this->auth = app()->make(AuthManager::class);
+        $this->expectException(ForestApiException::class);
+        $this->expectExceptionMessage(ErrorMessages::INVALID_STATE_MISSING);
+        $data = ['code' => 'test'];
+        $this->invokeMethod($this->auth, 'stateIsValid', array(&$data));
+    }
+
+    /**
+     * @throws BindingResolutionException
+     * @throws \ReflectionException
+     * @return void
+     */
+    public function testRenderingStateException(): void
+    {
+        /** @var AuthManager auth */
+        $this->auth = app()->make(AuthManager::class);
+        $this->expectException(ForestApiException::class);
+        $this->expectExceptionMessage(ErrorMessages::INVALID_STATE_RENDERING_ID);
+        $data = ['code' => 'test', 'state' => '{}'];
+        $this->invokeMethod($this->auth, 'stateIsValid', array(&$data));
+    }
+
+    /**
+     * @param string $return
+     * @param false  $withAuthorization
+     * @return ObjectProphecy
+     */
+    private function makeOidc(string $return, $withAuthorization = false): ObjectProphecy
+    {
+        $resourceOwner = $this->prophesize(ForestResourceOwner::class);
+        $resourceOwner
+            ->makeJwt()
+            ->willReturn($return);
+
+        $provider = $this->prophesize(ForestProvider::class);
+        $provider
+            ->getAccessToken(Argument::type('string'), Argument::size(2))
+            ->willReturn(
+                new AccessToken(['access_token' => 'token'])
+            );
+        $provider
+            ->getResourceOwner(Argument::any())
+            ->willReturn(
+                $resourceOwner->reveal()
+            );
+        $provider
+            ->setRenderingId(Argument::type('integer'))
+            ->willReturn($provider);
+
+        if ($withAuthorization) {
+            $provider->getAuthorizationUrl(Argument::any())
+                ->shouldBeCalled()
+                ->willReturn(
+                    'http://localhost/oidc/auth?state=%7B%22renderingId%22%3A1%7D&scope=openid%20profile%20email&response_type=code&approval_prompt=auto&redirect_uri=http%3A%2F%2Flocalhost%2Ffoo%2Fauthentication%2Fcallback&client_id=TEST'
+                );
+        }
+
+        $oidc = $this->prophesize(OidcClientManager::class);
+        $oidc
+            ->getClientForCallbackUrl(Argument::type('string'))
+            ->shouldBeCalled()
+            ->willReturn(
+                $provider->reveal()
+            );
+
+        return $oidc;
+    }
+}
