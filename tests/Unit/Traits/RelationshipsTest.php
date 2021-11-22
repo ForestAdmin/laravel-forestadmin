099660426db6f773c74696d2f9a6b79b36976f87
Matt <matthv@gmail.com>
2021-11-22 16:25:00 +0100

    chore: test mapping relationship of model

--- tests/Unit/Traits/RelationshipsTest.php
+++ tests/Unit/Traits/RelationshipsTest.php
@@ -2,7 +2,7 @@
 
 namespace ForestAdmin\LaravelForestAdmin\Tests\Unit\Traits;
 
-use ForestAdmin\LaravelForestAdmin\Schema\Concerns\DataTypes;
+use ForestAdmin\LaravelForestAdmin\Schema\Concerns\Relationships;
 use ForestAdmin\LaravelForestAdmin\Tests\TestCase;
 
 /**
@@ -12,21 +12,21 @@ use ForestAdmin\LaravelForestAdmin\Tests\TestCase;
  * @license GNU https://www.gnu.org/licenses/licenses.html
  * @link    https://github.com/ForestAdmin/laravel-forestadmin
  */
-class DataTypesTest extends TestCase
+class RelationshipsTest extends TestCase
 {
     /**
      * @throws \ReflectionException
      * @return void
      */
-    public function testGetType(): void
+    public function testMapRelationships(): void
     {
-        $trait = $this->getObjectForTrait(DataTypes::class);
-        $types = $this->invokeProperty($trait, 'dbTypes');
+        $trait = $this->getObjectForTrait(Relationships::class);
+        $types = $this->invokeProperty($trait, 'doctrineTypes');
 
         foreach ($types as $key => $value) {
-            $getType = $this->invokeMethod($trait, 'getType', [$key]);
+            $getType = $this->invokeMethod($trait, 'mapRelationships', [$key]);
 
-            $this->assertEquals($value, $getType);
+            $this->assertEquals($types[$key], $getType);
         }
     }
 }
