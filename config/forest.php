c8895d9bbf1d2dc90e88fbe254c619ad2b2c4ba4
Matt <matthv@gmail.com>
2021-12-01 09:59:11 +0100

    chore: update configuration add models_namespace

--- config/forest.php
+++ config/forest.php
@@ -2,6 +2,7 @@
 
 return [
     'models_directory' => env('MODEL_DIRECTORY', 'app/Models/'),
+    'models_namespace' => env('MODEL_NAMESPACE', 'App\Models\\'),
     'json_file_path'   => env('JSON_FILE_PATH', '.forestadmin-schema.json'),
     'api'              => [
         'url'         => env('FOREST_URL', 'https://api.forestadmin.com'),
