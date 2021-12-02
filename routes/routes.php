5df2e6d6242bd0add61144e672a74381700623f4
Matt <matthv@gmail.com>
2021-12-02 15:36:35 +0100

    chore: add configuration routes

--- routes/routes.php
+++ routes/routes.php
@@ -2,6 +2,7 @@
 
 use ForestAdmin\LaravelForestAdmin\Http\Controllers\ApiMapsController;
 use ForestAdmin\LaravelForestAdmin\Http\Controllers\AuthController;
+use ForestAdmin\LaravelForestAdmin\Http\Controllers\DispatchGateway;
 use Illuminate\Support\Facades\Route;
 
 Route::group(
@@ -13,5 +14,14 @@ Route::group(
         Route::post('authentication', [AuthController::class, 'login'])->name('forest.auth.login');
         Route::get('authentication/callback', [AuthController::class, 'callback'])->name('forest.auth.callback');
         //Route::get('custom-route', fn() => view('welcome'));
+
+        // CRUD
+        Route::get('/{collection}', DispatchGateway::class)->name('forest.collection.index');
+        Route::get('/{collection}/count', DispatchGateway::class)->name('forest.collection.count');
+        Route::get('/{collection}/{id}', DispatchGateway::class)->name('forest.collection.show');
+        Route::post('/{collection}', DispatchGateway::class)->name('forest.collection.store');
+        Route::put('/{collection}', DispatchGateway::class)->name('forest.collection.update');
+        Route::delete('/{collection}', DispatchGateway::class)->name('forest.collection.destroy_bulk');
+        Route::delete('/{collection}/{id}', DispatchGateway::class)->name('forest.collection.destroy');
     }
 );
