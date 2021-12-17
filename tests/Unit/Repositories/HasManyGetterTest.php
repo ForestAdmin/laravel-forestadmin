9db93e71b3eb069d38a70b9fa78a52f8f0dc80b7
Matt <matthv@gmail.com>
2021-12-17 12:10:29 +0100

    fix(getter): fix HasManyGetter related data

--- tests/Unit/Repositories/HasManyGetterTest.php
+++ tests/Unit/Repositories/HasManyGetterTest.php
@@ -5,6 +5,7 @@ namespace ForestAdmin\LaravelForestAdmin\Tests\Unit\Repositories;
 use ForestAdmin\LaravelForestAdmin\Repositories\HasManyGetter;
 use ForestAdmin\LaravelForestAdmin\Tests\Feature\Models\Book;
 use ForestAdmin\LaravelForestAdmin\Tests\Feature\Models\Comment;
+use ForestAdmin\LaravelForestAdmin\Tests\Feature\Models\Range;
 use ForestAdmin\LaravelForestAdmin\Tests\TestCase;
 use ForestAdmin\LaravelForestAdmin\Tests\Utils\FakeData;
 use Illuminate\Contracts\Pagination\LengthAwarePaginator;
@@ -35,6 +36,22 @@ class HasManyGetterTest extends TestCase
 
         $this->assertInstanceOf(LengthAwarePaginator::class, $data);
         $this->assertEquals(Comment::first(), $data->items()[0]);
+    }
+
+    /**
+     * @return void
+     */
+    public function testAllWithBelongsToManyRelation(): void
+    {
+        $this->getBook()->save();
+        $this->getRanges();
+
+        $repository = m::mock(HasManyGetter::class, [Book::first(), 'Book', 'ranges', 'Range', 1])
+            ->makePartial();
+        $data = $repository->all();
+
+        $this->assertInstanceOf(LengthAwarePaginator::class, $data);
+        $this->assertEquals(Range::first()->getAttributes(), $data->items()[0]->getAttributes());
     }
 
     /**
