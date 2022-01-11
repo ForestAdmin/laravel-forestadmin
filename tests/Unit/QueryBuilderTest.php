c4391aca08137ac856b43fdc0f4b64db56915afb
Matt <matthv@gmail.com>
2022-01-11 18:26:48 +0100

    chore: add test on QueryBuilder

--- tests/Unit/QueryBuilderTest.php
+++ tests/Unit/QueryBuilderTest.php
@@ -10,9 +10,11 @@ use Doctrine\DBAL\Types\Type;
 use ForestAdmin\LaravelForestAdmin\Services\QueryBuilder;
 use ForestAdmin\LaravelForestAdmin\Tests\Feature\Models\Book;
 use ForestAdmin\LaravelForestAdmin\Tests\TestCase;
+use ForestAdmin\LaravelForestAdmin\Tests\Utils\FakeData;
 use Illuminate\Database\Connection;
 use Illuminate\Database\Eloquent\Builder as EloquentQueryBuilder;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Query\Builder;
 use Mockery as m;
 use Prophecy\Argument;
 use Prophecy\PhpUnit\ProphecyTrait;
@@ -27,6 +29,7 @@ use Prophecy\PhpUnit\ProphecyTrait;
 class QueryBuilderTest extends TestCase
 {
     use ProphecyTrait;
+    use FakeData;
 
     /**
      * @return void
@@ -73,8 +76,7 @@ class QueryBuilderTest extends TestCase
     {
         $data = ['fields' => ['model' => 'foo,bar']];
         $model = $this->getLaravelModel();
-        $queryBuilder = m::mock(QueryBuilder::class, [$model, $data])
-            ->makePartial();
+        $queryBuilder = m::mock(QueryBuilder::class, [$model, $data])->makePartial();
 
         $fields = $queryBuilder->handleFields($model, $data['fields']['model']);
 
@@ -112,10 +114,111 @@ class QueryBuilderTest extends TestCase
             ->makePartial();
         $columns = $queryBuilder->getColumns($model);
 
-        $this->assertEquals(['id', 'foo', 'bar'], array_keys($columns));
+        $this->assertEquals(['id', 'foo', 'bar', 'uuid'], array_keys($columns));
         $this->assertEquals(Type::getType('bigint'), $columns['id']->getType());
         $this->assertEquals(Type::getType('string'), $columns['foo']->getType());
         $this->assertEquals(Type::getType('string'), $columns['bar']->getType());
+    }
+
+    /**
+     * @return void
+     * @throws Exception
+     * @throws SchemaException
+     */
+    public function testHandleSearchFieldrOnNumber(): void
+    {
+        $field = 'id';
+        $value = '1';
+        $handleSearchInt = $this->handleSearchField($field, 'Number', $value);
+        $expectResult = [
+            'type'     => 'Basic',
+            'column'   => 'dummy_tables.' . $field,
+            'operator' => '=',
+            'value'    => (int) $value,
+            'boolean'  => 'or',
+        ];
+
+        $this->assertEquals($expectResult, $handleSearchInt->getQuery()->wheres[0]);
+    }
+
+    /**
+     * @return void
+     * @throws Exception
+     * @throws SchemaException
+     */
+    public function testHandleSearchFieldOnEnum(): void
+    {
+        $field = 'foo';
+        $value = 'TEST';
+        $handleSearchEnum = $this->handleSearchField($field, 'Enum', $value);
+        $expectResult = [
+            'type'     => 'Basic',
+            'column'   => 'dummy_tables.' . $field,
+            'operator' => '=',
+            'value'    => $value,
+            'boolean'  => 'or',
+        ];
+
+        $this->assertEquals($expectResult, $handleSearchEnum->getQuery()->wheres[0]);
+    }
+
+    /**
+     * @return void
+     * @throws Exception
+     * @throws SchemaException
+     */
+    public function testHandleSearchFieldOnUuid(): void
+    {
+        $field = 'uuid';
+        $value = 'AA111111-AAAA-1111-1111-11AA11AA11AA';
+        $handleSearchUuid = $this->handleSearchField($field, 'String', $value);
+        $expectResult = [
+            'type'     => 'Basic',
+            'column'   => 'dummy_tables.' . $field,
+            'operator' => '=',
+            'value'    => $value,
+            'boolean'  => 'or',
+        ];
+
+        $this->assertEquals($expectResult, $handleSearchUuid->getQuery()->wheres[0]);
+    }
+
+
+    /**
+     * @return void
+     * @throws Exception
+     * @throws SchemaException
+     */
+    public function testHandleSearchFieldOnString(): void
+    {
+        $field = 'bar';
+        $value = 'Test value';
+        $handleSearchString = $this->handleSearchField($field, 'String', $value);
+        $expectResult = [
+            'type'    => 'raw',
+            'sql'     => 'LOWER (dummy_tables.bar) LIKE LOWER(?)',
+            'boolean' => 'or',
+        ];
+
+        $this->assertEquals($expectResult, $handleSearchString->getQuery()->wheres[0]);
+    }
+
+    /**
+     * @param $field
+     * @param $fieldType
+     * @param $value
+     * @return mixed
+     * @throws Exception
+     * @throws SchemaException
+     */
+    public function handleSearchField($field, $fieldType, $value)
+    {
+        $model = $this->getLaravelModel();
+        $field = ['field' => $field, 'type' => $fieldType];
+        $data = ['search' => $value];
+        $queryBuilder = m::mock(QueryBuilder::class, [$model, $data])->makePartial();
+        $builder = new EloquentQueryBuilder(new Builder($model->getConnection()));
+        return $queryBuilder->handleSearchField($builder, $model, $field, $value);
     }
 
     /**
@@ -129,9 +232,10 @@ class QueryBuilderTest extends TestCase
         $schemaManager->listTableColumns(Argument::any(), Argument::any())
             ->willReturn(
                 [
-                    'id'  => new Column('id', Type::getType('bigint')),
-                    'foo' => new Column('foo', Type::getType('string')),
-                    'bar' => new Column('bar', Type::getType('string')),
+                    'id'   => new Column('id', Type::getType('bigint')),
+                    'foo'  => new Column('foo', Type::getType('string')),
+                    'bar'  => new Column('bar', Type::getType('string')),
+                    'uuid' => new Column('foo', Type::getType('string')),
                 ]
             );
 
@@ -141,6 +245,10 @@ class QueryBuilderTest extends TestCase
             ->willReturn('prefix.');
         $connection->getDoctrineSchemaManager()
             ->willReturn($schemaManager->reveal());
+        $connection->getQueryGrammar()
+            ->willReturn(null);
+        $connection->getPostProcessor()
+            ->willReturn(null);
 
         $model = $this->prophesize(Model::class);
         $model
