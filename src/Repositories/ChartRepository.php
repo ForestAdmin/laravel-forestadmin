34eabaf22103dde97af4766f5eef2118419e0688
Matt <matthv@gmail.com>
2022-02-07 16:08:58 +0100

    feat(chart): add method on ChartRepository

--- src/Repositories/ChartRepository.php
+++ src/Repositories/ChartRepository.php
@@ -2,7 +2,11 @@
 
 namespace ForestAdmin\LaravelForestAdmin\Repositories;
 
+use Doctrine\DBAL\Exception;
+use ForestAdmin\LaravelForestAdmin\Services\QueryBuilder;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 /**
  * Class ChartRepository
@@ -11,14 +15,51 @@ use Illuminate\Database\Eloquent\Model;
  * @license GNU https://www.gnu.org/licenses/licenses.html
  * @link    https://github.com/ForestAdmin/laravel-forestadmin
  */
-class ChartRepository extends BaseRepository
+abstract class ChartRepository extends BaseRepository
 {
+    /**
+     * @var array
+     */
+    protected array $params;
+
+    /**
+     * @var string
+     */
+    protected string $aggregate;
+
+    /**
+     * @var string
+     */
+    protected string $aggregateField;
+
     /**
      * @param Model $model
      */
     public function __construct(Model $model)
     {
-        $this->params = request()->all();
         parent::__construct($model);
+        $this->params = request()->except('type', 'collection', 'aggregate', 'aggregate_field');
+        $this->aggregate = Str::lower(request()->input('aggregate'));
+        $this->aggregateField = request()->input('aggregate_field', '*');
+    }
+
+    /**
+     * @return array
+     */
+    abstract public function get(): array;
+
+    /**
+     * @param $data
+     * @return array
+     */
+    abstract public function serialize($data): array;
+
+    /**
+     * @return Builder
+     * @throws Exception
+     */
+    protected function query(): Builder
+    {
+        return QueryBuilder::of($this->model, $this->params);
     }
 }
