1877b7505d17dea468f4ae7e5b4cdbff39c78f5a
Matt <matthv@gmail.com>
2022-02-09 12:37:30 +0100

    feat(charts): update control of chart's type

--- src/Http/Controllers/ChartsController.php
+++ src/Http/Controllers/ChartsController.php
@@ -6,6 +6,7 @@ use ForestAdmin\LaravelForestAdmin\Exceptions\ForestException;
 use ForestAdmin\LaravelForestAdmin\Facades\JsonApi;
 use ForestAdmin\LaravelForestAdmin\Transformers\ChartTransformer;
 use ForestAdmin\LaravelForestAdmin\Utils\Traits\Schema;
+use Illuminate\Http\JsonResponse;
 use Ramsey\Uuid\Uuid;
 
 /**
@@ -20,19 +21,47 @@ class ChartsController extends ForestController
     use Schema;
 
     /**
-     * @return void
+     * @var string
+     */
+    protected string $type;
+
+    /**
+     * ChartsController constructor
+     */
+    public function __construct()
+    {
+        $this->validateRequestType();
+        $this->type = request()->input('type');
+    }
+
+    /**
+     * @return JsonResponse
      * @throws \Exception
      */
-    public function index()
+    public function index(): JsonResponse
     {
-        $type = request()->input('type');
-        if (!$type || !in_array($type, ['Type', 'Value', 'Pie', 'Objective', 'Leaderboard'], true)) {
-            throw new ForestException('The type of chart is not recognized.');
-        }
-
         $name = request()->route()->parameter('collection');
         $model = Schema::getModel(ucfirst($name));
-        $repository = new ('\ForestAdmin\LaravelForestAdmin\Repositories\Charts\Simple\\' . $type)($model);
+        $repository = new ('\ForestAdmin\LaravelForestAdmin\Repositories\Charts\Simple\\' . $this->type)($model);
+
+        return response()->json(
+            JsonApi::renderItem(
+                [
+                    'id'    => Uuid::uuid4(),
+                    'value' => $repository->get(),
+                ],
+                'stats',
+                ChartTransformer::class
+            )
+        );
+    }
+
+    /**
+     * @return JsonResponse
+     */
+    public function liveQuery(): JsonResponse
+    {
+        $repository = new ('\ForestAdmin\LaravelForestAdmin\Repositories\Charts\LiveQuery\\' . $this->type)();
 
         return response()->json(
             JsonApi::renderItem(
@@ -48,27 +77,11 @@ class ChartsController extends ForestController
 
     /**
      * @return void
-     * @throws \Exception
      */
-    public function liveQuery()
+    protected function validateRequestType(): void
     {
-        $type = request()->input('type');
-        if (!$type || !in_array($type, ['Type', 'Value', 'Pie', 'Objective', 'Leaderboard'], true)) {
-            throw new ForestException('The type of chart is not recognized.');
+        if (!request()->input('type') || !in_array(request()->input('type'), ['Type', 'Value', 'Pie', 'Objective', 'Leaderboard', 'Line'], true)) {
+            throw new ForestException("The chart's type is not recognized.");
         }
-
-        $repository = new ('\ForestAdmin\LaravelForestAdmin\Repositories\Charts\LiveQuery\\' . $type)();
-
-
-        return response()->json(
-            JsonApi::renderItem(
-                [
-                    'id'    => Uuid::uuid4(),
-                    'value' => $repository->get(),
-                ],
-                'stats',
-                ChartTransformer::class
-            )
-        );
     }
 }
