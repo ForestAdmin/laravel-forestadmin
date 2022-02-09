43b89d7db25bdccbb9732d586fad98bb4b286b40
Matt <matthv@gmail.com>
2022-02-09 15:29:38 +0100

    feat(charts): add class to handle Line Chart

--- src/Repositories/Charts/Simple/Line.php
+++ src/Repositories/Charts/Simple/Line.php
@@ -2,7 +2,11 @@
 
 namespace ForestAdmin\LaravelForestAdmin\Repositories\Charts\Simple;
 
+use Doctrine\DBAL\Exception;
 use ForestAdmin\LaravelForestAdmin\Repositories\ChartRepository;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
 
 /**
  * Class Line
@@ -13,5 +17,65 @@ use ForestAdmin\LaravelForestAdmin\Repositories\ChartRepository;
  */
 class Line extends ChartRepository
 {
+    /**
+     * @return array
+     * @throws Exception
+     */
+    public function get(): array
+    {
+        $groupBy = $this->handleGroupByField(request()->input('group_by_date_field'));
+        if ($this->aggregate === 'count') {
+            $this->aggregateField = $groupBy['field'];
+        } else {
+            $this->aggregateField = $this->table . '.' . $this->aggregateField;
+        }
 
+        $query = $this->query()->select(DB::raw($this->aggregate . '(' . $this->aggregateField . ')'), $groupBy['field'])
+            ->groupBy($groupBy['field'])
+            ->get()
+            ->mapWithKeys(fn($item, $key) => [Arr::get($item, $groupBy['responseField'])->format($this->getFormat()) => $item->{$this->aggregate}])
+            ->all();
+
+        return $this->serialize($query);
+    }
+
+    /**
+     * @param $data
+     * @return array
+     */
+    public function serialize($data): array
+    {
+        $result = [];
+        foreach ($data as $key => $value) {
+            $result[] = [
+                'label'  => $key,
+                'values' => compact('value'),
+            ];
+        }
+
+        return $result;
+    }
+
+    /**
+     * @return string
+     */
+    public function getFormat(): string
+    {
+        switch (Str::lower($this->params['time_range'])) {
+            case 'day':
+                $format = 'd/m/Y';
+                break;
+            case 'week':
+                $format = '\WW-Y';
+                break;
+            case 'month':
+                $format = 'M Y';
+                break;
+            case 'year':
+                $format = 'Y';
+                break;
+        }
+
+        return $format;
+    }
 }
