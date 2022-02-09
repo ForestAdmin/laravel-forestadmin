e889f5e0d738003aee277185407160e013d70156
Nicolas Alexandre <nicolasalexandre9@gmail.com>
2022-02-09 12:20:28 +0100

    feat(charts): added liveQuery value

---
+++ src/Repositories/Charts/LiveQuery/Value.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace ForestAdmin\LaravelForestAdmin\Repositories\Charts\LiveQuery;
+
+use ForestAdmin\LaravelForestAdmin\Repositories\LiveQueryRepository;
+
+/**
+ * Class Value
+ *
+ * @package  Laravel-forestadmin
+ * @license  GNU https://www.gnu.org/licences/licences.html
+ * @link     https://github.com/ForestAdmin/laravel-forestadmin
+ */
+class Value extends LiveQueryRepository
+{
+    /**
+     * @param $data
+     * @return array
+     */
+    public function serialize($data): array
+    {
+        $this->abortIf(!isset($data->first()->value), $data, "'value'");
+
+        return [
+            'countCurrent'  => $data->first()->value,
+            'countPrevious' => null,
+        ];
+    }
+}
