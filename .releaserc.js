19a1349fb8eddefd0b5c84ef510aa3125b106ff2
Jeff LADIRAY <ladirayjeff@gmail.com>
2022-03-09 14:40:25 +0100

    chore: enable semantic-release on the repository (#20)

---
+++ .releaserc.js
@@ -0,0 +1,78 @@
+module.exports = {
+  branches: ["main"],
+  plugins: [
+    [
+      "@semantic-release/commit-analyzer",
+      {
+        releaseRules: [
+          // This rule allow to force a release by adding "force-release" in scope.
+          // Example: `chore(force-release): support new feature`
+          // Source: https://github.com/semantic-release/commit-analyzer#releaserules
+          { scope: "force-release", release: "patch" },
+        ],
+      },
+    ],
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/changelog",
+    [
+      "@semantic-release/exec",
+      {
+        prepareCmd:
+          'sed -i "s/LIANA_VERSION = \'.*\'/LIANA_VERSION = \'${nextRelease.version}\'/g" src/Schema/Schema.php; sed -i \'s/"version": ".*"/"version": "${nextRelease.version}"/g\' composer.json; sed -i \'s/"version": ".*"/"version": "${nextRelease.version}"/g\' package.json;',
+      },
+    ],
+    [
+      "@semantic-release/git",
+      {
+        assets: ["CHANGELOG.md", "composer.json", "package.json"],
+      },
+    ],
+    "@semantic-release/github",
+    [
+      "semantic-release-slack-bot",
+      {
+        markdownReleaseNotes: true,
+        notifyOnSuccess: true,
+        notifyOnFail: false,
+        onSuccessTemplate: {
+          text: "📦 $package_name@$npm_package_version has been released!",
+          blocks: [
+            {
+              type: "section",
+              text: {
+                type: "mrkdwn",
+                text: "*New `$package_name` package released!*",
+              },
+            },
+            {
+              type: "context",
+              elements: [
+                {
+                  type: "mrkdwn",
+                  text: "📦  *Version:* <$repo_url/releases/tag/v$npm_package_version|$npm_package_version>",
+                },
+              ],
+            },
+            {
+              type: "divider",
+            },
+          ],
+          attachments: [
+            {
+              blocks: [
+                {
+                  type: "section",
+                  text: {
+                    type: "mrkdwn",
+                    text: "*Changes* of version $release_notes",
+                  },
+                },
+              ],
+            },
+          ],
+        },
+        packageName: "laravel-forestadmin",
+      },
+    ],
+  ],
+};
