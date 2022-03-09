ac510ba6726e02e1c4106c1af0b8ece848615664
Jeff LADIRAY <jeffl@forestadmin.com>
2022-03-09 15:50:03 +0100

    chore(force-release): add slack notification with correct versionning number

--- .releaserc.js
+++ .releaserc.js
@@ -33,51 +33,51 @@ module.exports = {
       },
     ],
     "@semantic-release/github",
-    // [
-    //   "semantic-release-slack-bot",
-    //   {
-    //     markdownReleaseNotes: true,
-    //     notifyOnSuccess: true,
-    //     notifyOnFail: false,
-    //     onSuccessTemplate: {
-    //       text: "📦 $package_name@$npm_package_version has been released!",
-    //       blocks: [
-    //         {
-    //           type: "section",
-    //           text: {
-    //             type: "mrkdwn",
-    //             text: "*New `$package_name` package released!*",
-    //           },
-    //         },
-    //         {
-    //           type: "context",
-    //           elements: [
-    //             {
-    //               type: "mrkdwn",
-    //               text: "📦  *Version:* <$repo_url/releases/tag/v$npm_package_version|$npm_package_version>",
-    //             },
-    //           ],
-    //         },
-    //         {
-    //           type: "divider",
-    //         },
-    //       ],
-    //       attachments: [
-    //         {
-    //           blocks: [
-    //             {
-    //               type: "section",
-    //               text: {
-    //                 type: "mrkdwn",
-    //                 text: "*Changes* of version $release_notes",
-    //               },
-    //             },
-    //           ],
-    //         },
-    //       ],
-    //     },
-    //     packageName: "laravel-forestadmin",
-    //   },
-    // ],
+    [
+      "semantic-release-slack-bot",
+      {
+        markdownReleaseNotes: true,
+        notifyOnSuccess: true,
+        notifyOnFail: false,
+        onSuccessTemplate: {
+          text: "📦 $package_name@$npm_package_version has been released!",
+          blocks: [
+            {
+              type: "section",
+              text: {
+                type: "mrkdwn",
+                text: "*New `$package_name` package released!*",
+              },
+            },
+            {
+              type: "context",
+              elements: [
+                {
+                  type: "mrkdwn",
+                  text: "📦  *Version:* <$repo_url/releases/tag/v$npm_package_version|$npm_package_version>",
+                },
+              ],
+            },
+            {
+              type: "divider",
+            },
+          ],
+          attachments: [
+            {
+              blocks: [
+                {
+                  type: "section",
+                  text: {
+                    type: "mrkdwn",
+                    text: "*Changes* of version $release_notes",
+                  },
+                },
+              ],
+            },
+          ],
+        },
+        packageName: "laravel-forestadmin",
+      },
+    ],
   ],
 };
