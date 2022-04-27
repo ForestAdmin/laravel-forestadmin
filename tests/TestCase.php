8c9b7fc6dedbb0643d465cf861761c41c1d33927
Nicolas Alexandre <nicolasalexandre9@gmail.com>
2022-04-27 11:40:42 +0200

    chore: remove unused config

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -94,7 +94,6 @@ class TestCase extends OrchestraTestCase
     {
         parent::getEnvironmentSetUp($app);
         $config = $app['config'];
-        $config->set('app.env', 'local');
         $config->set('app.debug', true);
         $config->set('forest.api.secret', 'my-secret-key');
         $config->set('forest.api.auth-secret', 'auth-secret-key');
