5f7de3022ff290d77158d00dbe1124c30bf69c0a
Matt <matthv@gmail.com>
2022-09-05 17:04:49 +0200

    fix(chart): allow handle chart on user has the correct permission level

--- src/Providers/AuthorizationProvider.php
+++ src/Providers/AuthorizationProvider.php
@@ -10,7 +10,6 @@ use Illuminate\Foundation\Support\Providers\AuthServiceProvider;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Gate;
-use Illuminate\Support\Str;
 
 /**
  * Class AuthorizationProvider
