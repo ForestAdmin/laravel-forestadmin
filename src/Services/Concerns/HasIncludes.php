f70a24f8dd6c98f97c5ca48a11eaf0f01bd1fe9f
Nicolas Alexandre <nicolasalexandre9@gmail.com>
2023-03-23 10:43:26 +0100

    feat: update appendRelation

--- src/Services/Concerns/HasIncludes.php
+++ src/Services/Concerns/HasIncludes.php
@@ -46,12 +46,19 @@ trait HasIncludes
             }
 
             if (isset($eagerLoads[$key])) {
-                $with = (new ReflectionFunction($closure))->getStaticVariables();
+                $with = (new ReflectionFunction($eagerLoads[$key]))->getStaticVariables();
                 $relation = $query->getModel()->$key();
                 $relationTable = $relation->getRelated()->getTable();
-                $fields = explode(',', Str::after($with['name'], "$key:"));
-                $includeFields = explode(',', Str::replace("$relationTable.", '', $value['fields']));
-                $value['fields'] = collect($fields)->merge($includeFields)->map(fn ($field) => "$relationTable.$field")->implode(',');
+                if ($with) {
+                    $fieldsRelationEagerLoad = explode(',', Str::after($with['name'], "$key:"));
+                    $includeFields = explode(',', Str::replace("$relationTable.", '', $value['fields']));
+                    $value['fields'] = collect($fieldsRelationEagerLoad)
+                        ->merge($includeFields)
+                        ->map(fn ($field) => "$relationTable.$field")
+                        ->implode(',');
+                } else {
+                    $value['fields'] = "$relationTable.*";
+                }
             }
 
             $query->with($key . ':' . $value['fields']);
