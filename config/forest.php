23345ea73ac5256f864bce9546951f5a8528acc3
Matt <matthv@gmail.com>
2023-06-26 16:56:40 +0200

    chore: set config packages

--- config/forest.php
+++ config/forest.php
@@ -1,6 +1,6 @@
 <?php
 
-//return [
+return [
 //    'models_directory'    => ['app/Models'], // TODO VOIR COMMENT GERER CE CAS
 //    'json_file_path'        => env('JSON_FILE_PATH', '.forestadmin-schema.json'),
 //    'send_apimap_automatic' => env('FOREST_SEND_APIMAP_AUTOMATIC', false),
@@ -11,24 +11,6 @@
 //    ],
 //    'included_models' => [],
 //    'excluded_models' => [],
-//];
+];
 
 
-
-
-// A ADAPTER AVEC LA CONFIGURATION SUIVANTE
-
-
-
-//return [
-//    'debug'                => Env::get('FOREST_DEBUG', true),
-//    'authSecret'           => Env::get('FOREST_AUTH_SECRET'),
-//    'envSecret'            => Env::get('FOREST_ENV_SECRET'),
-//    'forestServerUrl'      => Env::get('FOREST_SERVER_URL', 'https://api.forestadmin.com'),
-//    'isProduction'         => Env::get('APP_ENV', 'dev') === 'prod',
-//    'prefix'               => Env::get('FOREST_PREFIX', 'forest'),
-//    'permissionExpiration' => Env::get('FOREST_PERMISSIONS_EXPIRATION_IN_SECONDS', 1),
-//    'cacheDir'             => $this->appKernel->getContainer()->getParameter('kernel.cache_dir') . '/forest',
-//    'schemaPath'           => $this->appKernel->getProjectDir() . '/.forestadmin-schema.json',
-//    'projectDir'           => $this->appKernel->getProjectDir(),
-//];
