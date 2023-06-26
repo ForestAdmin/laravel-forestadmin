cfb579609197c0ff2efbb69dc7da7bc05c32ea5e
Matt <matthv@gmail.com>
2023-06-26 17:26:57 +0200

    chore: add new provider to handle agent php

---
+++ src/Providers/AgentProvider.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace ForestAdmin\LaravelForestAdmin\Providers;
+
+use ForestAdmin\AgentPHP\Agent\Builder\AgentFactory;
+use ForestAdmin\AgentPHP\Agent\Http\Router as AgentRouter;
+use Illuminate\Routing\Route;
+use Illuminate\Routing\RouteCollection;
+use Illuminate\Routing\Router;
+use Illuminate\Support\ServiceProvider;
+
+class AgentProvider extends ServiceProvider
+{
+    protected RouteCollection $routes;
+
+    public function boot()
+    {
+        $this->app->bind(AgentFactory::class, fn () => new AgentFactory($this->loadOptions()));
+        $this->loadConfiguration();
+        $this->app->get(AgentFactory::class)->build();
+        $this->loadRoutes();
+    }
+
+    /**
+     * @return RouteCollection
+     */
+    public function loadRoutes()
+    {
+        $prefix = '/forest';
+
+        foreach (AgentRouter::getRoutes() as $agentRoute) {
+            $this->app['router']->addRoute(['GET'], $prefix . $agentRoute['uri'], $agentRoute['closure']);
+        }
+    }
+
+    private function loadConfiguration(): void
+    {
+        if (file_exists(base_path() . '/forest/symfony_forest_admin.php')) {
+            $callback = require base_path() . '/forest/symfony_forest_admin.php';
+            $callback($this);
+        }
+    }
+
+    private function loadOptions(): array
+    {
+        return [
+            'debug'                => env('FOREST_DEBUG', true),
+            'authSecret'           => env('FOREST_AUTH_SECRET'),
+            'envSecret'            => env('FOREST_ENV_SECRET'),
+            'forestServerUrl'      => env('FOREST_SERVER_URL', 'https://api.forestadmin.com'),
+            'isProduction'         => env('APP_ENV', 'dev') === 'prod',
+            'prefix'               => env('FOREST_PREFIX', 'forest'),
+            'permissionExpiration' => env('FOREST_PERMISSIONS_EXPIRATION_IN_SECONDS', 1),
+            'cacheDir'             => storage_path('framework/cache/data/forest'),
+            'schemaPath'           => base_path() . '/.forestadmin-schema.json',
+            'projectDir'           => base_path(),
+        ];
+    }
+}
