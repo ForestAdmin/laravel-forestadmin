9c8ce8052cc595ae67d2da71058e8af6cb5c8e09
Matt <matthv@gmail.com>
2023-06-27 14:33:52 +0200

    chore: remove useless command forest clear + update main provider

--- src/ForestServiceProvider.php
+++ src/ForestServiceProvider.php
@@ -2,43 +2,24 @@
 
 namespace ForestAdmin\LaravelForestAdmin;
 
-use ForestAdmin\LaravelForestAdmin\Commands\ForestClear;
 use ForestAdmin\LaravelForestAdmin\Commands\ForestInstall;
 use ForestAdmin\LaravelForestAdmin\Commands\SendApimap;
 use ForestAdmin\LaravelForestAdmin\Http\Middleware\ForestCors;
-use ForestAdmin\LaravelForestAdmin\Providers\AuthorizationProvider;
+use ForestAdmin\LaravelForestAdmin\Providers\AgentProvider;
 use ForestAdmin\LaravelForestAdmin\Providers\EventProvider;
-use ForestAdmin\LaravelForestAdmin\Providers\RouteServiceProvider;
-use ForestAdmin\LaravelForestAdmin\Services\ChartApiResponse;
-use ForestAdmin\LaravelForestAdmin\Services\ForestSchemaInstrospection;
-use ForestAdmin\LaravelForestAdmin\Services\JsonApiResponse;
 use Illuminate\Contracts\Http\Kernel;
 use Illuminate\Support\ServiceProvider;
 
-/**
- * Class ForestServiceProvider
- *
- * @package Laravel-forestadmin
- * @license GNU https://www.gnu.org/licenses/licenses.html
- * @link    https://github.com/ForestAdmin/laravel-forestadmin
- */
 class ForestServiceProvider extends ServiceProvider
 {
-    /**
-     * Bootstrap the application events.
-     * @param Kernel $kernel
-     * @return void
-     */
     public function boot(Kernel $kernel): void
     {
+        $this->app->register(AgentProvider::class);
         $this->app->register(EventProvider::class);
-        //$this->app->register(AuthorizationProvider::class);
-        $this->app->register(RouteServiceProvider::class);
 
         if ($this->app->runningInConsole()) {
             $this->commands(
                 [
-                    ForestClear::class,
                     ForestInstall::class,
                     SendApimap::class,
                 ]
@@ -53,10 +34,6 @@ class ForestServiceProvider extends ServiceProvider
         );
 
         $kernel->pushMiddleware(ForestCors::class);
-
-        //$this->app->bind('chart-api', fn() => new ChartApiResponse());
-        //$this->app->bind('forest-schema', fn() => new ForestSchemaInstrospection()); -> A EVENTUELLEMENT FAIRE POINTER SUR LE FORESTSCHEMAINTROSPECTION DE L'AGENT
-        //$this->app->bind('json-api', fn() => new JsonApiResponse()); -> IDEM
     }
 
     /**
@@ -65,17 +42,6 @@ class ForestServiceProvider extends ServiceProvider
     public function register(): void
     {
         $this->mergeConfigFrom($this->configFile(), 'forest');
-
-//        config(
-//            [
-//                'auth.guards.forest' => array_merge(
-//                    [
-//                        'driver' => 'forest-token',
-//                    ],
-//                    config('auth.guards.forest', [])
-//                ),
-//            ]
-//        );
     }
 
     /**
