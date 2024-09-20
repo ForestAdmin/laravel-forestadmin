640437623772c3292ea8cc43257b1f9dd044b27e
Matt <matthv@gmail.com>
2024-09-20 10:03:36 +0200

    chore: lint

--- agentTemplate/forest_admin.php
+++ agentTemplate/forest_admin.php
@@ -1,12 +1,11 @@
 <?php
 
-use ForestAdmin\AgentPHP\Agent\Builder\AgentFactory;
 use ForestAdmin\AgentPHP\DatasourceEloquent\EloquentDatasource;
 use ForestAdmin\LaravelForestAdmin\Providers\AgentProvider;
 
 return static function () {
     $defaultDB = config('database.default');
-    $forestAgent =  AgentProvider::getAgentInstance();
+    $forestAgent = AgentProvider::getAgentInstance();
 
     $forestAgent->addDatasource(
         new EloquentDatasource(config('database.connections.' . $defaultDB)),
